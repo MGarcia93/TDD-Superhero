4add41ffa659b8999f63c8f955570cc29afa9923
“MGarcia93” <maximiliano_garcia93@hotmail.com>
2022-10-03 23:02:07 -0300

    create test createSuperhero in error

---
+++ tests/Feature/SuperheroTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enum\Creator;
+use App\Models\Superhero;
+use App\Models\Superpower;
+use Database\Seeders\SuperpowerSeeder;
+use Faker\Factory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class SuperheroTest extends TestCase
+{
+    use RefreshDatabase;
+    protected $faker;
+
+    /**
+     * Sets up the tests
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = Factory::create();
+
+        Artisan::call('migrate'); // runs the migration
+        $this->seed(SuperpowerSeeder::class);
+    }
+
+    /** @test*/
+    public function canCreateASuperHero()
+    {
+        $superpowers = Superpower::inRandomOrder()
+            ->limit($this->faker->numberBetween(1, 3))
+            ->get();
+        $data = [
+            'name' => $this->faker->sentence,
+            'history' => $this->faker->paragraph,
+            'creator' => $this->faker->randomElements(array_column(Creator::cases(), 'value')),
+            'SuperPower' => array_map(function ($power) {
+                return $power['id'];
+            }, $superpowers->toArray())
+        ];
+
+        $response = $this->json('POST', '/api/superhero', $data);
+
+        $response->assertStatus(201)
+            ->assertJson(compact('data'));
+        $this->assertDatabaseHas('superheros', [
+            'name' => $data['name'],
+            'history' => $data['history'],
+            'creator' => $data['creator']
+        ]);
+        $hero = Superhero::where('name', $data['name'])->where('history', $data['history'])->get();
+        array_map(function ($power) use ($hero) {
+            $this->assertDatabaseHas('superpowers_superheroes', [
+                'superpower_id' => $power['id'],
+                'superhero_id' => $hero->id
+            ]);
+        }, $superpowers->toArray());
+    }
+
+
+    /**
+     * Rolls back migrations
+     */
+    public function tearDown(): void
+    {
+        Artisan::call('migrate:rollback');
+
+        parent::tearDown();
+    }
+}
