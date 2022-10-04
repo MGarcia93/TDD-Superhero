833e4fa2ac58a3f069078fe95edd8ef54aca1b9d
“MGarcia93” <maximiliano_garcia93@hotmail.com>
2022-10-03 21:49:19 -0300

    create enum creator and superpower

---
+++ app/Enum/Creator.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum Creator: string
+{
+    case DC = 'DC Comics';
+    case Marvel = 'Marvel Comics';
+}
---
+++ app/Enum/SuperPower.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+enum SuperPower: string
+{
+    case Flight = "Flight";
+    case Invulnerabitlity = 'Invulnerabitlity';
+    case Agility = 'Agility';
+    case Strength = 'strength';
+}
